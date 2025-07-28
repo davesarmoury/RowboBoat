93f0459ec30492bc434179c5e4bc03e00ce5b4f6
Dave Niewinski <davesarmoury@gmail.com>
2025-07-28 14:08:25 -0400

    Initial description and viz packages

---
+++ rowboboat_description/launch/description.launch.py
@@ -0,0 +1,30 @@
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    package_path = get_package_share_path('piper_description')
+    default_model_path = package_path / 'urdf/piper_no_gripper_description.urdf'
+
+    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+                                      description='Absolute path to robot urdf file')
+
+    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+                                       value_type=str)
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher_node,
+    ])
---
+++ rowboboat_viz/launch/view_model.launch.py
@@ -0,0 +1,44 @@
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    package_path = get_package_share_path('piper_description')
+    default_rviz_config_path = package_path / 'rviz/piper_no_gripper.rviz'
+
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
+                                     description='Absolute path to rviz config file')
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+    )
+
+    description_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [PathJoinSubstitution([FindPackageShare("rowboboat_description"), "launch", "description.launch.py"])]
+        ),
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return LaunchDescription([
+        rviz_arg,
+        description_launch,
+        joint_state_publisher_gui_node,
+        rviz_node
+    ])
