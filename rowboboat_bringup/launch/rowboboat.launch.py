bea36adbb15cfef719e5faf47be9f02a5a6d01d6
Dave Niewinski <davesarmoury@gmail.com>
2025-08-07 13:41:21 -0400

    Added delay after arm launches.  I was going to put some stuff here, but changed my mind

--- rowboboat_bringup/launch/rowboboat.launch.py
+++ rowboboat_bringup/launch/rowboboat.launch.py
@@ -1,24 +1,14 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    desc_launch = IncludeLaunchDescription(
-        PathJoinSubstitution([PathJoinSubstitution([FindPackageShare('rowboboat_description'), 'launch']), 'description.launch.py'])
-    )
+    arms_launch = IncludeLaunchDescription(PathJoinSubstitution([PathJoinSubstitution([FindPackageShare('rowboboat_bringup'), 'launch']), 'arms.launch.py']))
 
-    arms_launch = IncludeLaunchDescription(
-        PathJoinSubstitution([PathJoinSubstitution([FindPackageShare('rowboboat_bringup'), 'launch']), 'arms.launch.py'])
-    )
-
-    control_launch = IncludeLaunchDescription(
-        PathJoinSubstitution([PathJoinSubstitution([FindPackageShare('rowboboat_control'), 'launch']), 'arms_control.launch.py'])
-    )
-
-    teleop_launch = IncludeLaunchDescription(
-        PathJoinSubstitution([PathJoinSubstitution([FindPackageShare('rowboboat_bringup'), 'launch']), 'teleop.launch.py'])
-    )
+    desc_launch = TimerAction(period=10.0, actions=[IncludeLaunchDescription(PathJoinSubstitution([PathJoinSubstitution([FindPackageShare('rowboboat_description'), 'launch']), 'description.launch.py']))])
+    control_launch = TimerAction(period=10.0, actions=[IncludeLaunchDescription(PathJoinSubstitution([PathJoinSubstitution([FindPackageShare('rowboboat_control'), 'launch']), 'arms_control.launch.py']))])
+    teleop_launch = TimerAction(period=10.0, actions=[IncludeLaunchDescription(PathJoinSubstitution([PathJoinSubstitution([FindPackageShare('rowboboat_bringup'), 'launch']), 'teleop.launch.py']))])
 
     # Return the LaunchDescription
     return LaunchDescription([
@@ -27,3 +17,4 @@ def generate_launch_description():
         control_launch,
         teleop_launch,
     ])
+
