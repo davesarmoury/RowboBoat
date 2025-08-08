631e0047e800342c3e615e73c5e91720e810df7f
Dave Niewinski <davesarmoury@gmail.com>
2025-08-08 12:43:14 -0400

    Fixed systemd since robot_upstart had weirdness with js permissions

--- rowboboat_bringup/systemd/INSTALL.sh
+++ rowboboat_bringup/systemd/INSTALL.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+## Setup CAN
 sudo cp rowboboat_can_activate.sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/rowboboat_can_activate.sh
 
 sudo cp rowboboat_can.service /etc/systemd/system
 sudo systemctl enable rowboboat_can.service
+
+## Start ROS
+sudo cp rowboboat.sh /usr/local/bin/
+sudo chmod +x /usr/local/bin/rowboboat.sh
+
+sudo cp rowboboat.service /etc/systemd/system
+sudo systemctl enable rowboboat.service
+
---
+++ rowboboat_bringup/systemd/rowboboat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /home/davesarmoury/ws/rowboboat_ws/install/setup.bash
+ros2 launch rowboboat_bringup rowboboat.launch.py
+
